44a60fe7e2e41a026195ed2d840dfb1337af9ed1
B4tiste <batiste.laloi@gmail.com>
2023-06-01 12:15:21 +0200

    Temperature + toggle leds

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
-from flask import Flask, Response, render_template
 import cv2
+import random
+from flask import Flask, Response, render_template, request
+
+num_leds = 5
+
+led_states = [random.choice([True, False]) for _ in range(num_leds)]
 
 app = Flask(__name__)
 
 # TODO : Services indépendants : cam et vidéo / Faire les petits services / Faire un readme
 
-def generate_frames():
+
+def generate_frames(mode):
     print('Starting camera...')
+
     # Path : C:\Users\batis\Desktop\ASI-Robotique\Partie2\static\videos\voitures.mp4
     while True:
-        # camera = cv2.VideoCapture(0)
-        camera = cv2.VideoCapture('static/videos/voitures.mp4')
+        if mode == "webcam":
+            camera = cv2.VideoCapture(0)
+        elif mode == "file":
+            camera = cv2.VideoCapture('static/videos/voitures.mp4')
         while True:
             success, frame = camera.read()
             if not success:
@@ -24,24 +33,44 @@ def generate_frames():
         camera.release()
 
 
+@app.route('/toggle_led', methods=['POST'])
+def toggle_led():
+    led_id = int(request.form.get('led_id'))
+    led_states[led_id] = not led_states[led_id]
+    print(f"Toggle led {led_id} to {led_states[led_id]}")
+    return {'new_state': led_states[led_id]}
+
+@app.route('/get_temperature', methods=['GET'])
+def get_temperature():
+    # Here I'm generating a random temperature for the sake of the example,
+    # but in a real scenario you would get this data from a real sensor.
+    temp = random.uniform(18, 23)
+    return {'temperature': temp}
+
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', num_leds=len(led_states), led_states=led_states)
 
 
-@app.route('/video')
-def video():
-    return render_template('video.html')
+@app.route('/video_webcam')
+def video_webcam():
+    return render_template('video_webcam.html')
 
 
-@app.route('/page2')
-def page2():
-    return render_template('page2.html')
+@app.route('/video_file')
+def video_file():
+    return render_template('video_file.html')
 
 
-@app.route('/video_stream')
-def video_stream():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/video_stream_webcam')
+def video_stream_webcam():
+    return Response(generate_frames("webcam"), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/video_stream_file')
+def video_stream_file():
+    return Response(generate_frames("file"), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
